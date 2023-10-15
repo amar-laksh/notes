f7ed4fcfc437a1b3cab46155c087880078309ab1
Amar Lakshya <amar.lakshya@protonmail.com>
2023-10-15 22:18:15 +0100

    adds lambda digraphs

--- functional/lisp/sicp/utils.rkt
+++ functional/lisp/sicp/utils.rkt
@@ -9,7 +9,7 @@
 (define head car)
 (define (tail x) (head (cdr x)))
 (define rest cdr)
-(define replicate (lambda (n x) (if (= n 0) '() (cons x (replicate (- n 1) x)))))
+(define replicate (λ (n x) (if (= n 0) '() (cons x (replicate (- n 1) x)))))
 
 
 (define (square x) (* x x))
